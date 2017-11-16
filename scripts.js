4150f8c01421e3b494c4f30a6c18b63692fe809c
Tyler Howard <tylerhoward@trailblazer.towson.edu>
2017-11-16 12:01:38 -0500

    Beautify actual codebase a bit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   AOS.init();
   if ($(window).width() < 768) {
     $(".fixed-top").addClass("bg-dark");
@@ -15,35 +15,39 @@ $(document).ready(function() {
     $("#resume").css("background-color", "lightblue");
   }
 
-  $('.navbar-brand').click(function(){
-      $('html,body').animate({ scrollTop: 0 }, 1000);
-      return false;
+  $('.navbar-brand').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
   });
-  $('.nav-link').click(function(){
-      var target = $(this).attr('href');
-      if(target.includes('#')){
-        var current = $(target);
-        if (target.length) {
-            var top = current.offset().top;
-            $('html,body').animate({scrollTop: top}, 1000);
-            return false;
-        }
+  $('.nav-link').click(function () {
+    var target = $(this).attr('href');
+    if (target.includes('#')) {
+      var current = $(target);
+      if (target.length) {
+        var top = current.offset().top;
+        $('html,body').animate({
+          scrollTop: top
+        }, 1000);
+        return false;
       }
+    }
   });
-  $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".fixed-top").addClass("bg-dark");
-        } else {
-            if ($(window).width() >= 768) {
-              $(".fixed-top").removeClass("bg-dark");
-            }
-        }
-    });
-    $(window).on('resize', function() {
-      if ($(window).width() < 768) {
-        $(".fixed-top").addClass("bg-dark");
+  $(window).scroll(function () {
+    if ($(".navbar").offset().top > 50) {
+      $(".fixed-top").addClass("bg-dark");
+    } else {
+      if ($(window).width() >= 768) {
+        $(".fixed-top").removeClass("bg-dark");
       }
-    });
+    }
+  });
+  $(window).on('resize', function () {
+    if ($(window).width() < 768) {
+      $(".fixed-top").addClass("bg-dark");
+    }
+  });
 
 
 });
