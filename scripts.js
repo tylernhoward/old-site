594dbea068382224c45f8ec0e50d46bdb9b23885
Tyler Howard <thowar4@students.towson.edu>
2018-01-30 21:19:55 -0500

    Add particle js for banner image

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
+
+
 $(document).ready(function () {
   AOS.init();
   if ($(window).width() < 768) {
-    $(".fixed-top").addClass("bg-dark");
+    $(".fixed-top").css("bg-custom");
 
   }
   if ($(window).width() < 400) {
@@ -9,10 +11,6 @@ $(document).ready(function () {
     $("#banner").css("height", "750px");
     $("#banner .overlay").css("width", "auto");
     $("#banner .overlay").css("height", "750px");
-    $("#banner").css("background", "none");
-    $("#resume").css("background", "none");
-    $("#banner").css("background-color", "lightblue");
-    $("#resume").css("background-color", "lightblue");
   }
 
   $('.navbar-brand').click(function () {
@@ -36,18 +34,122 @@ $(document).ready(function () {
   });
   $(window).scroll(function () {
     if ($(".navbar").offset().top > 50) {
-      $(".fixed-top").addClass("bg-dark");
+      $(".fixed-top").addClass("bg-custom");
     } else {
       if ($(window).width() >= 768) {
-        $(".fixed-top").removeClass("bg-dark");
+        $(".fixed-top").removeClass("bg-custom");
       }
     }
   });
   $(window).on('resize', function () {
     if ($(window).width() < 768) {
-      $(".fixed-top").addClass("bg-dark");
+      $(".fixed-top").addClass("bg-custom");
     }
   });
+});
 
-
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 150,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 20
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1.5,
+        "opacity_min": 0.15,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.5,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0.15,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 110,
+      "color": "#33b1f8",
+      "opacity": 0.25,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.6,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
 });
